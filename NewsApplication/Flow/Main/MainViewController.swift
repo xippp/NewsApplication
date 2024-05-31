aed11f7296e43750450f9815c405f345e5cf2230
xippp <paulasder@outlook.com>
2024-05-31 19:11:42 +0700

    Done: Setup Project with out storyboard

---
+++ NewsApplication/Flow/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  NewsApplication
+//
+//  Created by Paul on 31/5/2567 BE.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("Hello Come To Main News page")
+        
+    }
+    
+    
+}
