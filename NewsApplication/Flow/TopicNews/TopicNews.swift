ba2f01fad988bf055d84a6fb2fafb09e178fdc39
xippp <paulasder@outlook.com>
2024-06-02 22:53:14 +0700

    Done: Create TopicNewsViewController

---
+++ NewsApplication/Flow/TopicNews/TopicNews.swift
@@ -0,0 +1,16 @@
+//
+//  TopicNews.swift
+//  NewsApplication
+//
+//  Created by Paul on 2/6/2567 BE.
+//
+
+import Foundation
+import UIKit
+class TopicNews: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
